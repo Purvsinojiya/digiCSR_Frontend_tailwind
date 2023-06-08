c1b3f7c1085abde28c227068e06c809a64f57ec0
“poorv <you@example.com>
2023-06-08 11:57:09 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-xl font-bold'>hello tailwindcss</h1>
+      <h1 className='text-xl font-bold'>hello tailwkvvvvvvvvvvvvvvvkbvvvindcss</h1>
     </div>
   );
 }
