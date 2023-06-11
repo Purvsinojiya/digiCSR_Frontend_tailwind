c0b24751ffa62eafadef41db7b654beb1c9132b5
“poorv <you@example.com>
2023-06-11 19:39:23 +0530

    ngo-registration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import CompanyResgistraion from './company/CompanyResgistraion';
+import Ngo_Registration from './company/Ngo_Registration';
 
 
 function App() {
   return (
     <div className="App">
-      <CompanyResgistraion/>
+       <Ngo_Registration/>
     </div>
   );
 }
--- src/company/Ngo_Registration.js
+++ src/company/Ngo_Registration.js
@@ -4,9 +4,10 @@ import logo from "./image_company-componant/image 7.png";
 import person from "./image_company-componant/Profile.png";
 import "../App.css";
 
-const CompanyRegistration = () => {
+function Ngo_Registration() {
   return (
-    <div className="h-screen w-screen flex relative justify-center align-center">
+    <div>
+      <div className="h-screen w-screen flex relative justify-center align-center">
       <img
         className="h-screen w-full flex absolute"
         src={background}
@@ -35,13 +36,13 @@ const CompanyRegistration = () => {
       </div>
       <div className=" h-4/5 w-4/5 :w-3/4 xl:w-3/4  flex absolute mt-20 flex-col overflow-hidden rounded-sm ">
         <div className="h-28 w-full  flex justify-center items-center bg-[#7ED1FF] lg:text-4xl md:text-4xl text-2xl font-[Poppins] not-italic font-normal drop-shadow-[0px 4px 10px rgba(0, 0, 0, 0.25)] ">
-          Company Registration Form
+          Ngo Registration Form
         </div>
         <div className="p-3 h-fit min-h-0 w-full  bg-white  overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-200 backdrop-blur-40  scrollbar-track-gray-100 ld:text-base">
           <form className="space-y-3">
             <div className="grid grid-cols-1 gap-4">
               <label className="flex justify-start space-y-2.5">
-                Company Details
+                Ngo information
               </label>
               <div className="relative">
                 <input
@@ -55,7 +56,7 @@ const CompanyRegistration = () => {
                   htmlFor="amountInput"
                   className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
                 >
-                  Company information
+                  Ngo Name
                 </label>
               </div>
             </div>
@@ -73,8 +74,53 @@ const CompanyRegistration = () => {
                   htmlFor="amountInput"
                   className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
                 >
-                  Company Summary
+                  Ngo Summary
                 </label>
+              </div>
+            </div>
+            <div className="grid md:grid-cols-2 gap-4 ">
+            <div className="grid grid-cols-1 gap-4">
+              <div className="relative">
+                <input
+                  id="sectorInput"
+                  className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
+                  type="email"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  htmlFor="sectorInput"
+                  className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
+                >
+                  CSR Budget of this Year
+                </label>
+              </div>
+            </div>
+
+            
+            
+
+            <div className="relative">
+              <div className="grid grid-cols-1">
+              <select
+                className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
+                name="cars"
+                id="cars"
+              >
+                <option
+                  className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
+                  value=""
+                  disabled
+                  selected
+                >
+                  Select a sector
+                </option>
+                <option value="volvo">All sector</option>
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+              </select>
               </div>
             </div>
             <div className="grid grid-cols-1 gap-4">
@@ -90,93 +136,17 @@ const CompanyRegistration = () => {
                   htmlFor="sectorInput"
                   className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
                 >
-                  Year of Establishment
+                  Area of Operation
                 </label>
               </div>
             </div>
-
-            <div className="grid grid-cols-1 gap-4">
-              <label className="flex justify-start space-y-2.5">
-                Address Details
-              </label>
-              <div className="grid sm:grid-cols-3 md:grid-cols-3 gap-4">
-                <div className="relative col-span-1">
-                  <input
-                    id="timelineInput"
-                    className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
-                    type="number"
-                    placeholder=" "
-                    required
-                  />
-                  <label
-                    htmlFor="timelineInput"
-                    className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
-                  >
-                    City
-                  </label>
-                </div>
-                <div className="relative col-span-1">
-                  <input
-                    id="stateInput"
-                    className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
-                    type="text"
-                    placeholder=" "
-                    required
-                  />
-                  <label
-                    htmlFor="stateInput"
-                    className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
-                  >
-                    Location of the Company
-                  </label>
-                </div>
-                <div className="relative col-span-1">
-                  <input
-                    id="pincodeInput"
-                    className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
-                    type="text"
-                    placeholder=" "
-                    required
-                  />
-                  <label
-                    htmlFor="pincodeInput"
-                    className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
-                  >
-                    Pincode
-                  </label>
-                </div>
-              </div>
-            </div>
-
-            <div className="grid md:grid-cols-1 gap-4">
-              <label className="flex justify-start space-y-2.5">
-                Contact Details
-              </label>
-            </div>
-
-            <div className="relative">
-              <div className="grid grid-cols-1">
-                <input
-                  id="emailInput"
-                  className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
-                  type="text"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  htmlFor="emailInput"
-                  className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
-                >
-                  Email
-                </label>
-              </div>
             </div>
 
             <div className="grid grid-cols-1 gap-4">
               <label className="flex justify-start">
-                Communication Person details
+              Board Member Of NGO :-
               </label>
-              <div className="grid md:grid-cols-2 gap-4">
+              <div className="grid md:grid-cols-1 gap-4">
               <div className="relative">
                 <input
                   id="rfpTitleInput"
@@ -189,29 +159,46 @@ const CompanyRegistration = () => {
                   htmlFor="rfpTitleInput"
                   className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
                 >
-                  Designation of Communication Person
+                  Name
                 </label>
               </div>
               <div className="relative">
-                <div className="grid grid-cols-1">
+                <div className="grid grid-cols-3 ld:flex md:justify-center">
+                
+                  <div className="flex gap-2 ml-3 sm:ml-10 md:ml-20 ld:ml-20">
+                    <input type="radio"></input>
+                    <p>male</p>
+                  </div>
+                  <div className="flex gap-1 ml-3 sm:ml-10 md:ml-20 ld:ml-20">
+                    <input type="radio"></input>
+                    <p>fimale</p>
+                  </div>
+                  <div className="flex gap-1 ml-3 sm:ml-10 md:ml-20 ld:ml-20">
+                    <input type="radio"></input>
+                    <p>Other</p>
+                   
+                
+                  </div>
+                </div>
+                </div>
+              </div>
+
+              <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
+                <div className="relative">
                   <input
-                    id="emailInput"
+                    id="rfpTitleInput"
                     className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
                     type="text"
                     placeholder=" "
                     required
                   />
                   <label
-                    htmlFor="emailInput"
+                    htmlFor="rfpTitleInput"
                     className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
                   >
-                    Communication Person Name
+                    DIN Number
                   </label>
                 </div>
-                </div>
-              </div>
-
-              <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
                 <div className="relative">
                   <input
                     id="rfpTitleInput"
@@ -239,99 +226,13 @@ const CompanyRegistration = () => {
                     htmlFor="rfpTitleInput"
                     className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
                   >
-                    Email
+                    Phone Number
                   </label>
                 </div>
               </div>
             </div>
 
-            <div className="grid grid-cols-1 gap-4">
-              <label className="flex justify-start">Sector</label>
-
-              <select
-                className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
-                name="cars"
-                id="cars"
-              >
-                <option
-                  className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
-                  value=""
-                  disabled
-                  selected
-                >
-                  Select a sector
-                </option>
-                <option value="volvo">All sector</option>
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="mercedes">Mercedes</option>
-                <option value="audi">Audi</option>
-              </select>
-              <label
-                htmlFor="rfpTitleInput"
-                className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
-              ></label>
-            </div>
-            <label className="flex justify-start space-y-2.5 ">
-              Company Details
-            </label>
-            <div className="grid md:grid-cols-4 ">
-              <div className="flex space-x-2.5">
-                <input type="checkbox"></input>
-                <p>80 G (for 50% tax benefits)</p>
-              </div>
-              <div className="flex space-x-2.5">
-                <input type="checkbox"></input>
-                <p>80 G (for 50% tax benefits)</p>
-              </div>
-              <div className="flex space-x-2.5">
-                <input type="checkbox"></input>
-                <p>80 G (for 50% tax benefits)</p>
-              </div>
-              <div className="flex space-x-2.5">
-                <input type="checkbox"></input>
-                <p>80 G (for 50% tax benefits)</p>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-1 gap-4">
-              <label className="flex justify-start">Select document</label>
-              <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
-                <div className="relative">
-                  <input
-                    id="rfpTitleInput"
-                    className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
-                    type="file"
-                    placeholder=" "
-                    required
-                  />
-                  <label
-                    htmlFor="rfpTitleInput"
-                    className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
-                  >
-                    Company Registration Certificate
-                  </label>
-                </div>
-
-                <div className="grid grid-cols-1 gap-4">
-                  <div className="relative">
-                    <input
-                      id="amountInput"
-                      className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:border-blue-500 transition-all duration-300"
-                      type="file"
-                      placeholder=" "
-                      required
-                    />
-                    <label
-                      htmlFor="amountInput"
-                      className="absolute left-2 top-2 text-gray-400 text-sm transition-all duration-300 origin-left transform scale-100 pointer-events-none"
-                    >
-                      Profile Picture
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
+           
             <button className="rounded bg-[#7ED1FF] w-full p-1.5 font-normal text-white  color-[#FFFFFF] text-center text-2xl">
               save
             </button>
@@ -339,7 +240,8 @@ const CompanyRegistration = () => {
         </div>
       </div>
     </div>
-  );
-};
+    </div>
+  )
+}
 
-export default CompanyRegistration;
+export default Ngo_Registration
