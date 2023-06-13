49eec22e9b09ab8c24b264fb4e1d89615b2d8e5b
“poorv <you@example.com>
2023-06-13 18:14:38 +0530

    rfp-request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Login from './company/Login';
 import Navbar from './company/Navbar';
+import Rfp_Request from './company/Rfp_Request';
 import Singup from './company/Singup';
 
 
 function App() {
   return (
     <div className="App">
-    <Login/>
+    <Rfp_Request/>
     </div>
   );
 }
